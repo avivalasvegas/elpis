1792cd246610324e28a0bc3a388ebda6a750b359
avivabuller <avivabuller@gmail.com>
2016-11-14 00:51:31 +0000

    first commit

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,15 @@
 class Task < ActiveRecord::Base
-	attr_accessible :due_date, :description, :name
+	#attr_accessible :description, :name
 	belongs_to :user
 
 	validates :user_id, presence: true
 	validates :content, presence: true
 
-	auto_html_for :content do
-		html_escape
-		image
-		youtube(:width => "100%", :height => 250, :autoplay => false)
-		link :target => "_blank", :rel => "nofollow"
-		simple_format
-	end
+	#auto_html_for :content do
+		#html_escape
+		#image
+		#youtube(:width => "100%", :height => 250, :autoplay => false)
+		#link :target => "_blank", :rel => "nofollow"
+		#simple_format
+	#end
 end
